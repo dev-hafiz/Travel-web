4f849157df41a1a113cce177a3b385c1389a942d
okshi <johnstarhafiz@gmail.com>
2023-03-03 17:32:42 +0600

    Setup folder structure for this app

---
+++ Desktop/Okshiify-Mobile-App/sources/theme/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  white: "#ffffff",
+  black: "#1E1E1E",
+  gray: "#FCFCFC",
+  springWood: "#F6F6F4",
+  purpleBrown: "#644236",
+  balticSea: "#2C2724",
+  umber: "#863125",
+  mushroom: "#BDA38F",
+  linen: "#F9EFED",
+};
---
+++ Desktop/Okshiify-Mobile-App/sources/theme/typography.js
@@ -0,0 +1,5 @@
+export const typography = {
+  primary: "InriaSerif-Regular",
+  secondary: "IstokWeb-Regular",
+  bold: "InriaSerif-Bold",
+};
